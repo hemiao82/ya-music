22ef553249e91b2750b69146e2e42a25fab7c641
yuan <76913289@qq.com>
2022-06-02 10:47:32 +0800

    第一次提交“

---
+++ src/main/java/com/example/yamusic/YaMusicApplication.java
@@ -0,0 +1,13 @@
+package com.example.yamusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YaMusicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YaMusicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/yamusic/YaMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.yamusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YaMusicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
